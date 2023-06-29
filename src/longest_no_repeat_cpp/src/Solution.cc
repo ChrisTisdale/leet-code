2f898cf982c0ad522a0000cc5e34a7d855dafaf1
Chris Tisdale <cjtisdale21@gmail.com>
2023-06-28 22:30:54 -0500

    Fixing warnings

--- src/longest_no_repeat_cpp/src/Solution.cc
+++ src/longest_no_repeat_cpp/src/Solution.cc
@@ -7,12 +7,12 @@ int Solution::lengthOfLongestSubstring(std::string s) {
     int start = 0;
     unsigned int data[8] = { };
 
-    for (int i = 0; i < s.length(); ++i) {
+    for (unsigned int i = 0; i < s.length(); ++i) {
         auto cast = static_cast<unsigned int>(s[i]);
         auto bin = (cast / 32);
         auto check = cast % 32;
         if (data[bin] & (1 << check)) {
-            for (int j = start; j < s.length(); ++j) {
+            for (unsigned int j = start; j < s.length(); ++j) {
                 auto c2 = static_cast<unsigned int>(s[j]);
                 auto bin2 = (c2 / 32);
                 auto c3 = c2 % 32;
