9996e403cc1fbc31e5fe9decad0b0979a9ae8093
Chris Tisdale <cjtisdale21@gmail.com>
2023-07-07 21:44:12 -0500

    Fixing tests and adding clion build

--- src/longest_no_repeat_cpp/tests/solution_tests.cc
+++ src/longest_no_repeat_cpp/tests/solution_tests.cc
@@ -1,55 +1,55 @@
 #include <gtest/gtest.h>
 #include "../src/Solution.h"
 
-TEST(BINNARY_SEARCH, Example1Test) {
+TEST(LONGEST_SUBSTRING, Example1Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("abcabcbb"));
     GTEST_ASSERT_EQ(3, result);
 }
 
-TEST(BINNARY_SEARCH, Example2Test) {
+TEST(LONGEST_SUBSTRING, Example2Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("bbbbb"));
     GTEST_ASSERT_EQ(1, result);
 }
 
-TEST(BINNARY_SEARCH, Example3Test) {
+TEST(LONGEST_SUBSTRING, Example3Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("pwwkew"));
     GTEST_ASSERT_EQ(3, result);
 }
 
-TEST(BINNARY_SEARCH, Example4Test) {
+TEST(LONGEST_SUBSTRING, Example4Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string(" -&${}kj3222"));
     GTEST_ASSERT_EQ(10, result);
 }
 
-TEST(BINNARY_SEARCH, Example5Test) {
+TEST(LONGEST_SUBSTRING, Example5Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("  &${}kj3222"));
     GTEST_ASSERT_EQ(9, result);
 }
 
-TEST(BINNARY_SEARCH, Example6Test) {
+TEST(LONGEST_SUBSTRING, Example6Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("dvdf"));
     GTEST_ASSERT_EQ(3, result);
 }
 
-TEST(BINNARY_SEARCH, CaseDoesntMatterTest) {
+TEST(LONGEST_SUBSTRING, CaseDoesntMatterTest) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("aAaAaAaAaA"));
     GTEST_ASSERT_EQ(2, result);
 }
 
-TEST(BINNARY_SEARCH, Example7Test) {
+TEST(LONGEST_SUBSTRING, Example7Test) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("tmmzuxt"));
     GTEST_ASSERT_EQ(5, result);
 }
 
-TEST(BINNARY_SEARCH, AllCharacterAndLettersTest) {
+TEST(LONGEST_SUBSTRING, AllCharacterAndLettersTest) {
     Solution sl;
     auto result = sl.lengthOfLongestSubstring(std::string("asdfghjkl;'qwertyuiop[]\\zxcvbnm,./1234567890-=!@#$%^&*()_+ASDFGHJKLQWERTYUIOPZXCVBNM"));
     GTEST_ASSERT_EQ(84, result);
